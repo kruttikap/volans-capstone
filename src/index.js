58835468a0cd97fbf932b1feb55ca78f5e4fac73
Kruttika <patwardhankruttika@gmail.com>
2019-11-04 18:29:16 +0530

    Minor Redux Bug Fixed

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import * as firebase from 'firebase';
 import {createStore} from 'redux';
 import rootReducer from './redux/rootReducer';
+import {Provider} from 'react-redux';
 
 const firebaseConfig = {
     apiKey: "AIzaSyDuscLxYQLKCui3duGTm18JSuVto7WryyM",
@@ -24,7 +25,8 @@ var database = firebase.database();
 
 let store = createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}><App/></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
